fb8b47b3687840fa6d06c624c3aad494bc298836
Munyaradzi Chikara <syntaxread3r@gmail.com>
2023-07-05 18:22:29 +0100

    start storing information in redux

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { API_KEY, test_key } from "@env";
+import { useDispatch } from "react-redux";
+import { setOrigin, setDestination } from "../slices/navSlice";
 
 const HomeScreen = () => {
+	const dispatch = useDispatch();
 	const data =
 		"https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png";
 
-	const backupData = '../assets/uber-logo.png';
+	const backupData = "../assets/uber-logo.png";
 
 	return (
 		<SafeAreaView style={tw`bg-white h-full`}>
@@ -19,11 +24,42 @@ const HomeScreen = () => {
 						resizeMode: "contain",
 					}}
 					source={{
-						uri: data?data:backupData,
+						uri: data ? data : backupData,
 					}}
 				/>
 
-				<NavOptions/>
+				<GooglePlacesAutocomplete
+					placeholder="Where from?"
+					styles={{
+						container: {
+							flex: 0,
+						},
+						textInput: {
+							fontSize: 18,
+						},
+					}}
+					query={{
+						key: API_KEY,
+						language: "en",
+					}}
+					onPress={(data, details = null) => {
+						dispatchEvent(
+							setOrigin({
+								location: details.geometry.location,
+								discription: data.description,
+							})
+						);
+
+						dispatch(setDestication(null));
+					}}
+					fetchDetails={true}
+					enablePoweredByContainer={false}
+					minLength={2}
+					nearbyPlacesAPI="GooglePlacesSearch"
+					debounce={400} // This is a delay which will be executed when the user stops typing for a certain amount of time, in this case it's 400 milliseconds
+				/>
+
+				<NavOptions />
 			</View>
 		</SafeAreaView>
 	);
