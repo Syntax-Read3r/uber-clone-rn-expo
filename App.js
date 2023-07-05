c912068f1870e6839a810e64f8cc6ca656172787
Munyaradzi Chikara <syntaxread3r@gmail.com>
2023-07-05 16:25:14 +0100

    add a bunch of dependencies and set up the environment for them

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 // screens import
 import HomeScreen from "./screens/HomeScreen";
@@ -10,9 +13,11 @@ import HomeScreen from "./screens/HomeScreen";
 export default function App() {
 	return (
 		<Provider store={store}>
-			<SafeAreaProvider>
-				<HomeScreen />
-			</SafeAreaProvider>
+			<NavigationContainer>
+				<SafeAreaProvider>
+					<HomeScreen />
+				</SafeAreaProvider>
+			</NavigationContainer>
 		</Provider>
 	);
 }
