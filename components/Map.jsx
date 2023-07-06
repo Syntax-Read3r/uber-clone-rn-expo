960e99f532a5fc8ec50fb9253d49d7f61b0970b3
Munyaradzi Chikara <syntaxread3r@gmail.com>
2023-07-06 13:14:08 +0100

    fix the "property" cannot be read error after attempting to add a destination

--- components/Map.jsx
+++ components/Map.jsx
@@ -3,36 +3,54 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import tw from "tailwind-react-native-classnames";
 import { useSelector } from "react-redux";
-import { selectOrigin } from "../slices/navSlice";
+import { selectDestination, selectOrigin } from "../slices/navSlice";
+import MapViewDirections from "react-native-maps-directions";
+import { API_KEY } from "@env";
 
 const Map = () => {
 	const origin = useSelector(selectOrigin);
-
+	console.log('2 --- Map', origin?.description);
+  
+	const destination = useSelector(selectDestination);
+	console.log('3 --- Map', destination?.description);
+  
 	return (
-		<MapView
-			style={tw`flex-1`}
-			mapType="mutedStandard"
-			initialRegion={{
-				latitude: origin.location.lat,
-				longitude: origin.location.lng,
-				latitudeDelta: 0.005,
-				longitudeDelta: 0.005,
+	  <MapView
+		style={tw`flex-1`}
+		mapType="mutedStandard"
+		initialRegion={{
+		  latitude: origin?.location.lat,
+		  longitude: origin?.location.lng,
+		  latitudeDelta: 0.005,
+		  longitudeDelta: 0.005,
+		}}
+	  >
+		{origin && destination && (
+		  <MapViewDirections
+			origin={origin?.description}
+			destination={destination?.description}
+			apikey={API_KEY}
+			strokeWidth={3}
+			strokeColor="black"
+		  />
+		)}
+  
+		{origin?.location && (
+		  <Marker
+			coordinate={{
+			  latitude: origin.location.lat,
+			  longitude: origin.location.lng,
 			}}
-		>
-			{origin?.location && (
-				<Marker
-					coordinate={{
-						latitude: origin.location.lat,
-						longitude: origin.location.lng,
-					}}
-					title="Origin"
-					description="origin.description"
-					identifier="origin"
-				/>
-			)}
-		</MapView>
+			title="Origin"
+			description={origin?.description}
+			identifier="origin"
+		  />
+		)}
+	  </MapView>
 	);
-};
+  };
+  
+  
 
 export default Map;
 
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -7,77 +7,73 @@ import { API_KEY } from "@env";
 import { useDispatch } from "react-redux";
 import { setOrigin, setDestination } from "../slices/navSlice";
 
+const data =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png";
+const backupData = "../assets/uber-logo.png";
+
 const HomeScreen = () => {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const data =
-		"https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png";
+    const handlePress = (data, details = null) => {
+        const { geometry: { location }, description } = details;
 
-	const backupData = "../assets/uber-logo.png";
+        dispatch(
+            setOrigin({
+                location,
+                description: data.description,
+            })
+        );
 
-	return (
-		<SafeAreaView style={tw`bg-white h-full`}>
-			<View style={tw`p-5`}>
-				<Image
-					style={{
-						width: 100,
-						height: 100,
-						resizeMode: "contain",
-					}}
-					source={{
-						uri: data ? data : backupData,
-					}}
-				/>
+        dispatch(setDestination(null));
+    };
 
-				<GooglePlacesAutocomplete
-					placeholder="Where from?"
-					styles={{
-						container: {
-							flex: 0,
-						},
-						textInput: {
-							fontSize: 18,
-						},
-					}}
-					query={{
-						key: API_KEY,
-						language: "en",
-					}}
-					onPress={(data, details = null) => {
-						dispatch(
-							setOrigin({
-								location: details.geometry.location,
-								description: data.description,
-							})
-						);
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: "contain",
+                    }}
+                    source={{
+                        uri: data || backupData,
+                    }}
+                />
 
-						const origin = {
-							location: details.geometry.location,
-							description: data.description,
-						};
+                <GooglePlacesAutocomplete
+                    placeholder="Where from?"
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        },
+                    }}
+                    query={{
+                        key: API_KEY,
+                        language: "en",
+                    }}
+                    onPress={handlePress}
+                    enablePoweredByContainer={false}
+                    fetchDetails
+                    returnKeyType="search"
+                    minLength={2}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                />
 
-						console.log("1 --- ORIGIN", origin);
-
-						// dispatch(setDestination(null));
-					}}
-					enablePoweredByContainer={false}
-					fetchDetails={true}
-					returnKeyType={"search"}
-					minLength={2}
-					nearbyPlacesAPI="GooglePlacesSearch"
-					debounce={400} // This is a delay which will be executed when the user stops typing for a certain amount of time, in this case it's 400 milliseconds
-				/>
-
-				<NavOptions />
-			</View>
-		</SafeAreaView>
-	);
+                <NavOptions />
+            </View>
+        </SafeAreaView>
+    );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-	text: {
-		color: "green",
-	},
+    text: {
+        color: "green",
+    },
 });
